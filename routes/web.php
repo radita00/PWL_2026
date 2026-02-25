ee9ce726ea2186d77ba33e03cbdcb24bbc31030c
Radita Yoga Anugrah <radita.yogaanugrah2@gmail.com>
2026-02-25 22:32:40 +0700

    praktikum route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+   return 'Hello World';
 });
+
+Route::get('/world', function () {
+   return 'World';
+});
+
+Route::get('/selamat', function () {
+   return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+   return 'Nama: Radita Yoga Anugrah <br> NIM: 244107020099';
+});
+
+// Route::get('/user/{name}', function ($name) {
+//    return 'Nama saya ' .$name;
+// });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+   return 'Pos ke-' .$postId. ' Komentar ke-' .$commentId;
+});
+
+Route::get('/artikel/{artikel}', function ($artikelId) {
+   return 'Halaman Artikel dengan ID ' .$artikelId;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+   return 'Nama saya: ' .$name;
+});
+
